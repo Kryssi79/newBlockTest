af3a4827a6e4c680068aa453f9d9a88987467814
Kryssi79 <KryssiListy@freenet.de>
2013-10-11 02:20:08 +0200

    Anfang ...

--- src/com/Kryssi/newBlockTest/main.java
+++ src/com/Kryssi/newBlockTest/main.java
@@ -1,6 +1,22 @@
 package com.Kryssi.newBlockTest;
 
+import org.bukkit.block.Block;
+import org.bukkit.plugin.java.JavaPlugin;
 
-public class main {
+
+public class main extends JavaPlugin 
+{
+	
+	@Override
+	public void onEnable()
+	{
+		System.out.println("starte:   newBlockTest - Plugin  ");
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		System.out.println("beende:   newBlockTest - Plugin  ");
+	}
 
 }
